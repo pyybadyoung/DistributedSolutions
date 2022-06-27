a7da1cf2d67116d5e28a0007bb1d54f9321cfdec
badyoung <m15955482913@qq.com>
2022-06-27 22:29:58 +0800

    分布式解决方案之分布式锁

--- src/main/java/com/pyy/distributedSolutions/distributedLock/redissonLock/RedissonController.java
+++ src/main/java/com/pyy/distributedSolutions/distributedLock/redissonLock/RedissonController.java
@@ -34,16 +34,39 @@ public class RedissonController {
     String port;
 
     @Resource
-    RedisTemplate<String,String> redisTemplate;
+    RedisTemplate<String, String> redisTemplate;
 
     private static final String MAOTAI = "maotai20210321001";//茅台商品编号
 
     private static final String RESOURCE_UNIQUE_IDENTIFIER = "423e08ec-a38e-4ff9-b928-1d2f98ee01b7";
 
+
+//    //单机
+//    RedissonClient redisson = Redisson.create();
+//    Config config = new Config();
+//    config.useSingleServer().setAddress("myredisserver:6379");
+//
+//
+//
+//    //主从
+//    config.useMasterSlaveServers().setMasterAddress("127.0.0.1:6379").addSlaveAddress("127.0.0.1:6389","127.0.0.1:6332","127.0.0.1:6419")
+//    .addSlaveAddress("127.0.0.1:6399");
+//
+//    //哨兵
+//    config.useSentinelServers().setMasterName("mymaster").addSentinelAddress("127.0.0.1:26389","127.0.0.1:26379").addSentinelAddress("127.0.0.1:26319");
+//
+//
+//
+//    //集群
+//    config.useClusterServers().setScanInterval(2000)// cluster state scan interval in milliseconds
+//    .addNodeAddress("127.0.0.1:7000","127.0.0.1:7001").addNodeAddress("127.0.0.1:7002");
+//
+
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://"+host+":"+port);
+        config.useSingleServer().setAddress("redis://" + host + ":" + port);
         return Redisson.create(config);
     }
 
@@ -53,10 +76,11 @@ public class RedissonController {
 
     /**
      * 使用Redisson框架实现分布式锁进行减库存操作
+     *
      * @return
      */
     @GetMapping("/seckillMaotaiV4")
-    public String seckillMaotaiV4(){
+    public String seckillMaotaiV4() {
         RLock lock = redissonClient.getLock(RESOURCE_UNIQUE_IDENTIFIER);
         lock.lock();
         try {
@@ -64,17 +88,18 @@ public class RedissonController {
             //如果还有库存
             if (count > 0) {
                 //抢到了茅台，库存减一
-                redisTemplate.opsForValue().set(MAOTAI,String.valueOf(count-1));
+                redisTemplate.opsForValue().set(MAOTAI, String.valueOf(count - 1));
                 //后续操作 do something
                 log.info("我抢到茅台了!");
                 return "ok";
-            }else {
+            } else {
                 return "no";
             }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            lock.unlock();;
+            lock.unlock();
+            ;
         }
         return "";
     }
